361257262dc5cba927248074abf7b5401d6b9cb5
Esthefany07 <teffarr@gmail.com>
2020-09-07 16:10:46 +0200

    peticiones de pedidos

--- models/admin.js
+++ models/admin.js
@@ -1,3 +1,15 @@
+const { get } = require("../routes");
+
+//METODO PARA OBTENER TODOS LOS PEDIDOS
+function getAllPedidos() {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT * FROM ifruit.pedidos', (error, rows) => {
+            if (error) return reject(error);
+            resolve(rows);
+        })
+    })
+}
+
 //METODO PARA OBTENER TODO EL LISTADO DE PEDIDOS PENDIENTES DE ENTREGA
 
 function getAllPedidosAdmin() {
@@ -8,6 +20,22 @@ function getAllPedidosAdmin() {
         })
     })
 };
+
+//METODO PARA CREAR UN NUEVO PEDIDO
+
+const nuevoPedido = ({ id_pedido, cantidad, fecha_entrega, precio_total, id_cliente }) => {
+
+    return new Promise((resolve, reject) => {
+        db.query('insert into pedidos(id_pedido, cantidad, fecha_entrega, precio_total, id_cliente values(?,?,?,?,?))',
+            [id_pedido, cantidad, fecha_entrega, precio_total, id_cliente], (error, result) => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            });
+    });
+
+}
 
 //METODO PARA OBTENER TODO EL LISTADO DE CLIENTES
 
@@ -21,4 +49,4 @@ function getAllClientes() {
 };
 
 
-module.exports = { getAllPedidosAdmin, getAllClientes }
+module.exports = { getAllPedidosAdmin, getAllClientes, getAllPedidos, nuevoPedido }

86c40950664258fda38fb2b6af5dc07f5c255f12
Esthefany07 <teffarr@gmail.com>
2020-09-15 18:01:27 +0200

    routas eliminadas

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const apiClientesRouter = require('./api/clientes');
 const apiAdminRouter = require('./api/admin');
 const apiProductosRouter = require('./api/productos');
 const apiPedidosRouter = require('./api/pedidos');
+const apiCartRouter = require('./api/cart');
 const { checkToken } = require('./middlewares');
 
 
@@ -13,4 +14,5 @@ router.use('/clientes', apiClientesRouter);
 router.use('/admin', apiAdminRouter);
 router.use('/productos', apiProductosRouter);
 router.use('/pedidos', apiPedidosRouter);
+router.use('/carrito', apiCartRouter);
 module.exports = router;
